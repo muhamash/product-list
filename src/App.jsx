3cff856c3ac9b3e04054cd8b01d8e63f35581c58
muhamsh <muhammad-ashraful@outlook.com>
2024-10-29 20:13:33 +0600

    footer on

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Nav from './components/Nav'
 
@@ -6,7 +7,8 @@ function App() {
   return (
     <>
       <Nav />
-      <Header/>
+      <Header />
+      <Footer/>
     </>
   )
 }
---
+++ src/components/ProductListTitle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ProductListTitle() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
