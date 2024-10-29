3668aab6a9f0160785163bedf50eac58325f1e74
muhamsh <muhammad-ashraful@outlook.com>
2024-10-29 22:59:31 +0600

    working on frontend

--- src/components/ProductSection.jsx
+++ src/components/ProductSection.jsx
@@ -1,11 +1,27 @@
 // import React from 'react'
-import ProductListTitle from './ProductListTitle'
+import Cart from './Cart';
+import Filter from './Filter';
+import ProductListTitle from './ProductListTitle';
+import Search from './Search';
+import Sort from './Sort';
 
 export default function ProductSection() {
     return (
         <div className="pt-16 sm:pt-24 lg:pt-40">
             <ProductListTitle />
-          
+            <div className="mt-10">
+                <div className="flex justify-between relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
+                    <div className='w-full'>
+                        <Sort />
+                        <Filter />
+                    </div>
+
+                    <div className="flex gap-2 items-center">
+                        <Search/>
+                        <Cart/>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
