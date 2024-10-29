60b2f9e99cc5ed52dd19fb082099c0ffd1e5fd5c
muhamsh <muhammad-ashraful@outlook.com>
2024-10-29 20:08:02 +0600

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Header from './components/Header'
+import Nav from './components/Nav'
 
 function App() {
   return (
-    <div className="bg-green-900 text-white">
-      hello
-    </div>
+    <>
+      <Nav />
+      <Header/>
+    </>
   )
 }
 
