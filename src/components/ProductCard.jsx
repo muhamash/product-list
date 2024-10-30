13f04a3f9c8ebbc385e5962fbb99eae497d44807
muhamsh <muhammad-ashraful@outlook.com>
2024-10-30 23:58:21 +0600

    product card updated image loading state added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,23 @@
 /* eslint-disable react/prop-types */
- 
-// import React from 'react';
+import { useState } from 'react';
 
-export default function ProductCard({image, title, category, price}) {
+export default function ProductCard({ image, title, category, price }) {
+    const [loading, setLoading] = useState(true);
+
     return (
         <div className="relative">
-            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none lg:h-80">
+            <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md lg:aspect-none lg:h-80 relative">
+                { loading && (
+                    <div className="absolute inset-0 flex items-center justify-center bg-gray-200 z-10">
+                        <div className="spinner border-t-4 border-b-4 border-teal-500 rounded-full w-12 h-12 animate-spin"></div>
+                    </div>
+                ) }
                 <img
-                    src={image}
-                    alt="product image?"
-                    className="h-full w-full object-cover object-top lg:h-full lg:w-full p-4 bg-gray-100"
+                    src={ image }
+                    alt="product image"
+                    className={ `h-full w-full object-cover object-top lg:h-full lg:w-full p-4 bg-gray-100 ${loading ? 'hidden' : 'block'
+                        }` }
+                    onLoad={ () => setLoading( false ) }
                 />
             </div>
             <div className="mt-4 px-3 pb-4">
@@ -17,12 +25,9 @@ export default function ProductCard({image, title, category, price}) {
                     <h3 className="text-sm text-gray-700">{ title }</h3>
                     <p className="mt-1 text-sm text-gray-500">{ category }</p>
                 </div>
-                <p className="text-sm font-medium text-gray-900">{  price }</p>
+                <p className="text-sm font-medium text-gray-900">{ price }</p>
             </div>
-            {/* <!-- Button --> */}
-            <div
-                className="cursor-pointer rounded-md bg-white text-[0.8125rem] font-medium leading-5 text-slate-700 ring-1  hover:ring-1 ring-slate-700/10 hover:bg-slate-50 hover:text-slate-900 items-center text-center mb-3 mx-3 flex-1"
-            >
+            <div className="cursor-pointer rounded-md bg-white text-[0.8125rem] font-medium leading-5 text-slate-700 ring-1 hover:ring-1 ring-slate-700/10 hover:bg-slate-50 hover:text-slate-900 items-center text-center mb-3 mx-3 flex-1">
                 <div className="flex px-3 py-2 justify-center">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
@@ -38,7 +43,6 @@ export default function ProductCard({image, title, category, price}) {
                             d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
                         />
                     </svg>
-
                     Add To Cart
                 </div>
             </div>
